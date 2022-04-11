fdaa590c01218631027f6bcbd91bbb4e6997b981
Faiz Maricar <maricar.faiz@gmail.com>
2022-04-11 23:44:14 +0800

    fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,5 +18,14 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+         `Jost`
+        ],
+        display: 'swap',
+      }
+    }
   ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,11 @@
 import React from "react"
 import Seo from "../components/Seo";
-import useSiteMetadata from "../hooks/useSiteMetadata"
 
 const IndexPage = () => {
-  const siteMetadata = useSiteMetadata();
-  console.log(siteMetadata)
   return (
   <>
     <Seo/>
-    <h1>Faiz Maricar</h1>
+    <h1 style={{fontFamily: 'Jost'}}>Faiz Maricar</h1>
   </>
 )}
 
